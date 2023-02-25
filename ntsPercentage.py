295b49892f0891d44844c58f0065b01b7c8bf25e
Silvia <silvia.carbonell@crg.eu>
2023-02-25 23:08:47 +0100

    add imput validation and code cleanup

--- ntsPercentage.py
+++ ntsPercentage.py
@@ -1,12 +1,28 @@
 # -*- coding: utf-8 -*-
 """
-Python script to compute the percentage of each nucleotide of a given DNA sequence
+Python script to compute the percentage of each nucleotide
+of a given DNA sequence
 
 """
+import sys
+import re
 
-seq = input("Type a sequence: ")
-total_bases = len (seq)
-for base in 'acgt':
-    percentage = ((seq.count(base) / total_bases)*100)
-    print ('The percentage of {0} is {1}'.format(base, percentage))
-    
+
+def validate_dna(seq):
+    if not re.search('^[ACGT]+$', seq):
+        print('ERROR, input needs to be DNA sequence', file=sys.stderr)
+        exit(1)
+
+
+def main():
+    seq = input("Type a sequence: ")
+    seq = seq.upper()
+    validate_dna(seq)
+    total_bases = len(seq)
+    for base in 'ACTG':
+        percentage = ((seq.count(base) / total_bases) * 100)
+        print('The percentage of {0} is {1}'.format(base,
+                                                    round(percentage, 3)))
+
+
+main()
